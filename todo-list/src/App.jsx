4ff12bc216becd49267ab3bd2b9fbbf2db017458
razyal8 <razyal8@gmail.com>
2023-08-17 13:50:19 +0300

    Start css and menu

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/home-page'; 
 import NoteGrid from './components/task-grid'
 import TaskForm from './components/task-form';
+import Header from './components/header';
 
 function App() {
   return(
     <BrowserRouter>
-      <Routes>
+      <Header className="header"/>
+      <Routes className="App">
         <Route path="/" element={<HomePage />}/>
         <Route path="/tasks" element={<TaskForm />}/>
         <Route path="/notes" element={<NoteGrid />}/>
