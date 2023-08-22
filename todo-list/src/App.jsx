8e0b0d72788a5d88b5a94bf43756e455ffaf3ed4
razyal8 <razyal8@gmail.com>
2023-08-22 11:56:25 +0300

    Fix css and add navbar

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -9,12 +9,14 @@ import Header from './components/header';
 function App() {
   return(
     <BrowserRouter>
+    <div className='App'>
       <Header className="header"/>
-      <Routes className="App">
+      <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/tasks" element={<TaskForm />}/>
         <Route path="/notes" element={<NoteGrid />}/>
       </Routes>
+    </div>
     </BrowserRouter>
     );
 }
